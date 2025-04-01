e282607b0f7da8ece97e1e9db7ae9ebe06fa95c1
Caike De Oliveira Domingues <caike.dom@gmail.com>
2025-04-01 16:42:01 -0300

    Aula 1 da imersão dev da aluta
    Conversor de moedas wons

---
+++ Conversor de wons - Aula 1/wons.js
@@ -0,0 +1,15 @@
+
+
+
+/*Comando que irá permitir que o usuário digite valores no programa, esse comando será atribuido a variável 'valor' que irá guardar os
+valores digitados pelos usuários. */
+valorwons = prompt("Digite um valor em wons: ")
+
+/*Irá guardar o valor atual da moeda won */
+wons = 0.0040
+
+/*Ira conter a multiplicação que irá resultar na conversão das moedas */
+resultado = valorwons * wons
+
+/*Irá gerar a janela com o resultado da conversão em wons */
+alert('Resultado da conversão ' + resultado)
---
+++ aula1.js
@@ -0,0 +1,8 @@
+
+
+
+/*Só é executado no console do google ou do terminal */
+console.log('Olá Mundo!')
+
+/*Só é executado no navegador */
+alert('Olá Mundo!')
